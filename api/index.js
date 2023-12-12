5bbefb1c5dfdd546be39ce8de5bcee40c570aac4
Edward <programmeredward@gmail.com>
2023-12-12 14:11:25 +0800

    created auth signup route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 dotenv.config()
 
 mongoose
@@ -11,8 +13,13 @@ mongoose
     .catch((err) => {
         console.log(err)
     })
-    
+
 const app = express()
+
+app.use(express.json())
+
+app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
 
 app.listen(3000, () => {
     console.log("Server Running on port 3000")
