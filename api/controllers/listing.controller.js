8783b22055bcf4a5288f642ee294bf373bc31c8b
Edward <programmeredward@gmail.com>
2023-12-21 13:04:01 +0800

    Updated header search functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -90,7 +90,7 @@ export const getListings = async (req, res, next) => {
             type = { $in: ['sale', 'rent'] }
         }
 
-        const searchTerm = req.quer.searchTerm || ''
+        const searchTerm = req.query.searchTerm || ''
 
         const sort = req.query.sort || 'createdAt'
 
